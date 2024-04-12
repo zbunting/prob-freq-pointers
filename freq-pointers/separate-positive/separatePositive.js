b326755ad69e9ff264e83841e31389f65621e509
Zach Bunting <zvrbunting@gmail.com>
2024-04-12 16:08:46 -0700

    Finished separatePositives.

--- freq-pointers/separate-positive/separatePositive.js
+++ freq-pointers/separate-positive/separatePositive.js
@@ -1,6 +1,45 @@
 // Add whatever parameters you deem necessary & write a docstring
+
+//eg [2,-1,-3,6] -> [2,6,-1,-3]
+//left = 0; right = 3
+
+
+//initialize a left pointer at 0
+//initialize a right pointer at the length of the array minus 1
+//iterate while left is less than right
+//if the left pointer is positive, increment it
+//if the right pointer is negative, decrement it
+//if the left pointer is negative and the right pointer is positive,
+//switch places and then increment left and decrement right
+//return the array
+
+/** Move the positive numbers in an array to the left and the negative
+ * numbers to the right
+ * Input: an array of numbers
+ * Output: the array sorted IN PLACE
+ * Time Complexity: O(n)
+ */
+
 function separatePositive(nums) {
-    return [0];
+    let left = 0;
+    let right = nums.length - 1;
+
+    while(left < right) {
+
+        if(nums[left] > 0) left++;
+
+        if(nums[right] < 0) right--;
+
+        if(nums[left] < 0 && nums[right] > 0) {
+
+            [nums[left],nums[right]] = [nums[right],nums[left]];
+            left++;
+            right--;
+
+        }
+    }
+
+    return nums;
 }
 
 export { separatePositive };
