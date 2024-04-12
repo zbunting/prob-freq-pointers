6ca641af76077b11921f33454709bff27e98f493
Zach Bunting <zvrbunting@gmail.com>
2024-04-12 16:45:31 -0700

    Finished twoArrayObject.

--- freq-pointers/two-array-object/twoArrayObject.js
+++ freq-pointers/two-array-object/twoArrayObject.js
@@ -1,5 +1,35 @@
+//eg ['x', 'y', 'z'], [1, 2]
+//{x: 1, y: 2, z: null}
+
+//eg ['a', 'b', 'c'], [1, 2, 3, 4]
+//{a: 1, b: 2, c: 3}
+
+//if there are more values than keys, ignore
+//loop through the keys
+//look at the first item of each array and turn those into key-value pairs
+//in the object
+//check if there is no value at that spot, substitute null
+//return the object
+
+/**
+ * Take 2 arrays and make a object from them
+ * Input: array of keys and an array of values
+ * Return: object made with the keys and values from the arrays
+ * Time Complexity: O(n)
+ */
+
 function twoArrayObject(keys, vals) {
-    return { "": null };
+    const object = {};
+
+    for (let i = 0; i < keys.length; i++) {
+        if (vals[i] === undefined) {
+            object[keys[i]] = null;
+        } else {
+            object[keys[i]] = vals[i];
+        }
+    }
+
+    return object;
 }
 
 export { twoArrayObject };
